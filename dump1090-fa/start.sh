7ae8a23db4eec0a81878a197bb5efc2e8750a1ac
ShoGinn <scottginn@Scotts-Mac-mini.local>
2018-11-12 12:41:24 -0800

    updated dump1090-fa

---
+++ dump1090-fa/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir /run/dump1090-fa
+
+/usr/sbin/service lighttpd start
+
+/usr/bin/dump1090-fa \
+--quiet \
+--net \
+--net-sbs-port $DUMP1090_SBS_PORT \
+--lat $DUMP1090_LAT \
+--lon $DUMP1090_LON \
+--write-json /run/dump1090-fa \
+--write-json-every 1 \
+--json-location-accuracy 2 &
+
+while true
+        do
+        sleep 20
+done
