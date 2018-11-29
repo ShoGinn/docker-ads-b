f65a06eb0665c6e97fa504dfa44b12f19b319452
ShoGinn <scottginn@Scotts-Mac-mini.local>
2018-11-28 17:57:22 -0800

    Update docker_push.sh

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -3,5 +3,5 @@
 set -o errexit
 set -o pipefail
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 TAG="${TRAVIS_BRANCH}" docker-compose push "${SUB_IMAGE}"
