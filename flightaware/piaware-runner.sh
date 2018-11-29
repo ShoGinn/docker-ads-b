4227d0052ed4e2ce83620323f4beb12ae2fe84d7
ShoGinn <scottginn@Scotts-Mac-mini.local>
2018-11-28 17:35:19 -0800

    revamp
    revamp

--- flightaware/piaware-runner.sh
+++ flightaware/piaware-runner.sh
@@ -14,27 +14,12 @@ echo
 echo "FLIGHTAWARE_FEEDER_ID=${PIAWARE_FEEDER_ID}"
 echo
 
-if [ -z "${DUMP1090_HOST}" ]; then
-	echo "No DUMP1090_HOST Set"
-else
- 	/usr/bin/piaware-config "receiver-host" "${DUMP1090_HOST}"
-fi
-
-if [ -z "${DUMP1090_PORT}" ]; then
-	echo "No DUMP1090_PORT Set"
-else
-	/usr/bin/piaware-config "receiver-port" "${DUMP1090_PORT}"
-fi
 if [ -z "${PIAWARE_FEEDER_ID}" ]; then
 	echo "No PIAWARE_FEEDER_ID set"
 else
 	/usr/bin/piaware-config "feeder-id" "${PIAWARE_FEEDER_ID}"
 fi
 
-if [ ! -z ${PIAWARE_GPS} ]; then
-    echo "Enabling GPS"
-    /usr/sbin/gpsd -n ${PIAWARE_GPS}
-fi
 # Fix issue with fa-mlat-client
 # The fa-mlat-client is run as "nobody" with most permissions dropped.
 # This causes issues with extracting to ~/.shiv (the default) so use /tmp instead.
--- planefinder/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/pfclient --address=$DUMP1090_HOST --port=$DUMP1090_PORT --sharecode=$PF_SHARECODE --lat=$DUMP1090_LAT --lon=$DUMP1090_LON --data_format=1
