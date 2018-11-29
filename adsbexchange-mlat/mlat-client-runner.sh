5e8a043f71ee87b3ee9c94ca8461aba223adcd80
ShoGinn <scottginn@Scotts-Mac-mini.local>
2018-11-28 19:56:17 -0800

    updates

--- adsbexchange-mlat/mlat-client-runner.sh
+++ adsbexchange-mlat/mlat-client-runner.sh
@@ -26,5 +26,5 @@ mlat-client --input-type 'dump1090' \
             --user "${MLAT_CLIENT_USER}" \
             --server 'feed.adsbexchange.com:31090' \
             --no-udp \
-            --results 'beast,listen,30104' \
+            --results 'beast,connect,${DUMP1090_SERVER}:30104' \
             "$@"
